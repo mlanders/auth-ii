72aa268567666bf1a362554610ff4d6b76ebc9b7
Mike Landers <landers.mike@gmail.com>
2019-02-28 14:35:33 -0800

    adjusting what gets sent back from the users db

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ server.post('/api/login', async (req, res) => {
 
 server.get('/api/users', restricted, async (req, res) => {
 	try {
-		const users = await db('users');
+		const users = await db('users').select('id', 'username', 'department');
 		res.status(200).json({ message: 'Success', users });
 	} catch {
 		res.status(500).json({ message: 'Unexpected error' });
